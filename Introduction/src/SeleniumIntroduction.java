70de9796ce753e21c67d44061de1efa4aed74347
Nikola_Lukovic <83911165+OPA1234@users.noreply.github.com>
2023-11-21 12:45:28 +0100

    New Selenium Java project Import classes for WebDriver and Browser

---
+++ Introduction/src/SeleniumIntroduction.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntroduction  {
+
+	public static void main(String[] args) {
+		
+		
+		//Invoking Browser
+		//Chrome - ChromeDriver --> Methods
+//		WebDriver driver = new ChromeDriver();
+		
+		//chromedriver.exe
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Documents\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		
+		
+		
+
+	}
+
+}
