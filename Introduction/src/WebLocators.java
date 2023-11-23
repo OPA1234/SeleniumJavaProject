ef71a91e055e524a1a41763b8762ba8fa63cbb35
Nikola_Lukovic <83911165+OPA1234@users.noreply.github.com>
2023-11-23 15:58:18 +0100

    WebLocators

---
+++ Introduction/src/WebLocators.java
@@ -0,0 +1,50 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebLocators {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("Rastko");
+        driver.findElement(By.name("inputPassword")).sendKeys("Asdf123");
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector(".error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Rastko");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rastko@nemanjici.com");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).clear();
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rastko123@nemanjici.com");
+        driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("+3816542321232");
+        driver.findElement(By.xpath("//button[normalize-space()='Reset Login']")).click();
+        System.out.println(driver.findElement(By.xpath("//p[@class='infoMsg']")).getText());
+        driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("inputUsername")).sendKeys("Rastko");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("signInBtn")).click();
+
+
+
+
+        
+
+
+        
+
+        
+
+
+
+       
+    }
+    
+}
